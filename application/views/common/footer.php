8e38cdc00c399b5255a9504dd224f93c0ec48488
abfs87 <reversedrug.arc@gmail.com>
2016-01-18 22:23:57 +0700

    Initial commit

---
+++ application/views/common/footer.php
@@ -0,0 +1,7 @@
+		</div>
+			<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css')?>">
+			<script src="<?php echo base_url('assets/js/jquery-2.1.4.min.js');?>"></script>
+			<script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
+			<script src="<?php echo base_url('assets/js/script.js');?>"></script>
+	</body>
+</html>
